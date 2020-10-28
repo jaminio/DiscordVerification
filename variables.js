f6fe2b43ee5ce059bc966398fb94546e43fe0853
jaminio <59372236+jaminio@users.noreply.github.com>
2020-10-28 22:56:29 +0000

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,13 +1,13 @@
 // Discord info
 const discord_bot_token = '';
-const discord_guild_id = '';
-const discord_verified_role = '';
+const discord_guild_id = '326773822650843136';
+const discord_verified_role = 'Verified';
 
 // reCAPTCHA v2 secret key
-const recaptcha_secret_key = '';
+const recaptcha_secret_key = '6Leqi9wZAAAAAOpmlbMJuojO2RKqs6TSQeCN07Cb';
 
 // Domain
-const domain = ''; // Leave empty for localhost
+const domain = 'verify.nova-hotels.com'; // Leave empty for localhost
 
 module.exports = {
     discord_bot_token,
